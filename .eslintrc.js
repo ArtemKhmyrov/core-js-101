6bde79a5521471652842c9208fb6e9b9486b930c
Хмыров А.М <a.hmyrov-saratov@mail.ru>
2023-09-27 22:38:22 +0400

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    'linebreak-style': 'off',
+    "linebreak-style": 0,
     "eol-last": 0,
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
   },
 };
